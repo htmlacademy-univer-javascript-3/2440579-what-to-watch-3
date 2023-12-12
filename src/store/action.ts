efca1717e5d368dad6a8874858e060c8e770a0e1
Konstantin Zaikin <zaikinke@yandex.ru>
2023-12-12 23:21:06 +0500

    Add task solution

--- src/store/action.ts
+++ src/store/action.ts
@@ -5,3 +5,5 @@ export const setGenre = createAction('set-genre', (genre: string) => ({
 }));
 
 export const getFilms = createAction('get-films');
+
+export const upFilmSize = createAction('up-film-size');
